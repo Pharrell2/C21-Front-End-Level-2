310d8dfb511176d4f6fce0dff67dca816e5addd4
Pharrell Helstone <2153880@talnet.nl>
2021-11-18 18:01:05 +0100

    Front end final version

---
+++ python/opdracht_05.py
@@ -0,0 +1,7 @@
+# Prints out the name of favorite game
+def favorite_game(name):
+    print("One of my favorite games is",name)
+
+favorite_game("Battlefront2")
+favorite_game("Gta5")
+favorite_game("Gran turismo")
